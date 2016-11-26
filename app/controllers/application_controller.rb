eddfc94cd287d5d280e35f2984239ffa5fbd41cd
Traci7822 <Tracir@gmail.com>
2016-11-25 20:54:33 -0700

    established root path and connection to layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,10 @@ class ApplicationController < ActionController::Base
   respond_to :json
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def index
+    render 'layouts/application'
+  end
+
   protected
 
     def configure_permitted_parameters
