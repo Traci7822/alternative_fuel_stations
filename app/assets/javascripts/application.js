e107ba2c9f312c448d9fedda7dcfb331757354b4
Traci7822 <Tracir@gmail.com>
2016-11-26 11:09:19 -0700

    set up angular

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,5 +11,9 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery_ujs
+//= require angular
+//= require angular-ui-router
+//= require angular-devise
+//= require angular-rails-templates
+//= require bootstrap-sprockets
 //= require_tree .
