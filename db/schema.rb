dfc26029c4b00e9778981dbb8ad9068e81c6a466
Traci7822 <Tracir@gmail.com>
2016-11-29 12:11:01 -0700

    setup bower and removed devise

---
+++ db/schema.rb
@@ -0,0 +1,42 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20161126032402) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "ratings", force: :cascade do |t|
+    t.integer  "station_id"
+    t.integer  "user_id"
+    t.integer  "stars"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "stations", force: :cascade do |t|
+    t.string   "fuel_type_code"
+    t.string   "ev_connector_types"
+    t.string   "name"
+    t.string   "street_address"
+    t.text     "phone"
+    t.text     "hours"
+    t.integer  "station_id"
+    t.text     "city"
+    t.text     "state"
+    t.integer  "zip"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+  end
+
+end
