906e51fce4c35ae66c285c49691523c1ef9aeadd
Traci7822 <Tracir@gmail.com>
2016-11-28 20:39:26 -0700

    added Register controller

--- app/assets/javascripts/controllers.js
+++ app/assets/javascripts/controllers.js
@@ -53,6 +53,26 @@ stationApp.controller('LoginController', ['$location', 'AuthenticationService', 
   }
 }])
 
+stationApp.controller('RegisterController', ['UserService', '$location', '$rootScope', 'FlashService', function(UserService, $location, $rootScope, FlashService) {
+  var vm = this;
+
+        vm.register = register;
+
+        function register() {
+            vm.dataLoading = true;
+            UserService.Create(vm.user)
+                .then(function (response) {
+                    if (response.success) {
+                        FlashService.Success('Registration successful', true);
+                        $location.path('/login');
+                    } else {
+                        FlashService.Error(response.message);
+                        vm.dataLoading = false;
+                    }
+                });
+        }
+ }])
+
 
 // function getClub(id) {
 //      return $http.get('clubs/' + $stateParams.id)
