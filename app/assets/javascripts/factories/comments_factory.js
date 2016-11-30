7813ebb5194b178090d553e2b4ad85ce34d980a1
Traci7822 <Tracir@gmail.com>
2016-11-30 10:57:50 -0700

    create comment post request is going through but not valid

--- app/assets/javascripts/factories/comments_factory.js
+++ app/assets/javascripts/factories/comments_factory.js
@@ -1,5 +1,24 @@
-stationApp.service('CommentService', function() {
-  this.createComment = function(comment) {
-    debugger;
-  }
-})
+stationApp.factory('CommentFactory', ['$http', function($http) {
+    return {
+      createComment: createComment
+    }
+
+    function createComment(comment) {
+      var req = {
+        method: 'POST',
+        url: 'http://localhost:3000/comments',
+        headers: {
+          'Content-Type': undefined
+        },
+        data: {commment: 'comment'}
+      }
+
+      return $http(req)
+        .then(function(data){
+          console.log(data)
+        })
+        .catch(function(data) {
+          console.log(data)
+        })
+      }  
+}])
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,11 @@
 class CommentsController < ApplicationController
   def index
+    binding.pry
     @comments = Comment.all
     render json: @comments
+  end
+
+  def create
+    binding.pry
   end
 end
