ceb6a5d534fe7b92d040294cf025c2a1be6f1102
Traci7822 <Tracir@gmail.com>
2016-12-03 11:12:03 -0700

    station controller is parsing data for pagination

--- app/assets/javascripts/controllers/station_controller.js
+++ app/assets/javascripts/controllers/station_controller.js
@@ -1,8 +1,28 @@
 stationApp.controller('StationController', ['$scope', 'StationService', '$stateParams', function($scope, StationService, stateParams) {
-  StationService
-    .getStations()
-    .then(function (res) {
-      $scope.stationList = res.data;
-    });
+  var ctrl = this;
+  $scope.filteredStations = [];
+  $scope.currentPage = 1;
+  $scope.numPerPage = 10;
+  $scope.maxSize = 5;
 
+  $scope.getStations = function() {
+    $scope.stationList = StationService.getStations().then(function (res) {
+      $scope.filterStations(res.data);
+    });
+  }
+
+  $scope.filterStations = function(stations) {
+    $scope.stations = stations;
+    $scope.$watch('currentPage + numPerPage', function() {
+      var begin = (($scope.currentPage - 1) * $scope.numPerPage);
+      var end = begin + $scope.numPerPage;
+      $scope.filteredStations = $scope.stations.slice(begin, end);
+    })
+  }
+
+  $scope.getStations();
 }]);
+
+//
+// for (var i = 0; i < res.data.length; i++){
+//   $scope.stationList.push(res.data[i])
