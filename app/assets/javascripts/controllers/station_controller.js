8a05a53d6e4059cca4930ca7a72257e15c381174
Traci7822 <Tracir@gmail.com>
2016-12-08 17:47:41 -0700

    table is sortable by header

--- app/assets/javascripts/controllers/station_controller.js
+++ app/assets/javascripts/controllers/station_controller.js
@@ -1,8 +1,12 @@
 stationApp.controller('StationController', ['$scope', 'StationFactory', '$stateParams', function($scope, StationFactory, stateParams) {
+  $scope.sortType = 'name';
+  $scope.sortReverse = false;
+
   StationFactory
     .getStations()
     .then(function (res) {
       $scope.stationList = res.data;
+      
     });
 
 }]);
