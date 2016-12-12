a766d1a1f265c459815aea9d938d27d2ce81f5c5
Traci7822 <Tracir@gmail.com>
2016-12-12 09:59:04 -0700

    adding bower components

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ Bundler.require(*Rails.groups)
 module AltFuelStations
   class Application < Rails::Application
     config.assets.initialize_on_precompile = false
+    config.assets.paths << Rails.root.join("vendor","assets","bower_components","bootstrap-sass-official","assets","fonts")
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
