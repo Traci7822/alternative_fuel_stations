c8f550d603cfc1e9fea13f76efbd4864e2f3cea1
Traci7822 <Tracir@gmail.com>
2016-12-12 08:55:04 -0700

    added vendor dependencies

--- config/initializers/bower_rails.rb
+++ config/initializers/bower_rails.rb
@@ -3,7 +3,8 @@ BowerRails.configure do |bower_rails|
     "vendor": {
       "name": "bower-rails generated vendor assets",
       "dependencies": {
-        "angularUtils-pagination": "latest"
+        "angularUtils-pagination": "latest",
+        "angular": "latest"
       }
     }
   }
