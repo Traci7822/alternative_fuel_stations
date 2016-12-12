cb84c01669fb8d34b33bae2ac4a785c191eaf886
Traci7822 <Tracir@gmail.com>
2016-12-12 08:59:11 -0700

    added vendor dependencies

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,9 @@ Bundler.require(*Rails.groups)
 module AltFuelStations
   class Application < Rails::Application
     config.assets.initialize_on_precompile = false
+    root.join('vendor', 'assets', 'bower_components').to_s.tap do |bower_path|
+      config.assets.paths << bower_path
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
