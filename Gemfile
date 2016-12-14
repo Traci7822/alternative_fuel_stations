3e9c42dd645df2a9fc9d7a04372c3e49444fd6bb
Traci7822 <Tracir@gmail.com>
2016-12-14 16:26:21 -0700

    assets

--- Gemfile
+++ Gemfile
@@ -34,12 +34,13 @@ gem 'bootstrap-sass', '~> 3.3.6'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
-gem 'rails_12factor', group: :production
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
   gem 'pry'
+  gem 'rails_12factor'
+
 end
 
 group :development do
--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 
 module AltFuelStations
   class Application < Rails::Application
-    config.serve_static_assets = true
+    config.serve_static_files = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
